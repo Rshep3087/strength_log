f2a313065155a690fd15e21091cfbad9cbf7f845
Ryan Sheppard <Rshep3087@gmail.com>
2020-03-03 03:08:37 +0000

    config from env

--- strength_log/config.py
+++ strength_log/config.py
@@ -1,14 +1,11 @@
 import os
+from loguru import logger
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    SECRET_KEY = "5791628bb0b13ce0c676dfde280ba245"
-    username = "rshep3087"
-    password = "KJNqp6FkVnJc9ONTpaOT"
-    hostname = "rshep3087.mysql.pythonanywhere-services.com"
-    databasename = "rshep3087$strength_log"
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@{hostname}/{databasename}"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                            'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_RECYCLE = 299
