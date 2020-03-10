8f15c4150ebc701abbe4811cae4ff0926cdcd361
Ryan Sheppard <Rshep3087@gmail.com>
2020-03-09 21:04:15 -0500

    add delete button to update post add modal to delete

--- strength_log/config.py
+++ strength_log/config.py
@@ -1,15 +1,12 @@
 import os
-from loguru import logger
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    logger.debug(SECRET_KEY)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "SQLALCHEMY_DATABASE_URI"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
-    logger.debug(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_POOL_RECYCLE = 299
