1f46478ee1689a782bd3ff5216c6e80b72e77fb1
Ryan Sheppard <Rshep3087@gmail.com>
2020-03-14 12:04:18 -0500

    add email to send errors

--- strength_log/__init__.py
+++ strength_log/__init__.py
@@ -6,6 +6,9 @@ from flask_migrate import Migrate
 from flask_mail import Mail
 from loguru import logger
 
+import logging
+from logging.handlers import SMTPHandler
+
 from strength_log.config import Config
 
 
@@ -48,3 +51,23 @@ def create_app(config_class=Config):
 
 
 app = create_app()
+
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="Strength Log Failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
