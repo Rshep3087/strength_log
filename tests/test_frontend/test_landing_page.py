eed8921e6f04305dbcc268091d99cf35d3c4d5f7
Ryan Sheppard <Rshep3087@gmail.com>
2020-03-21 12:15:29 -0500

    create test classes for frontend tests test user logging in

--- tests/test_frontend/test_landing_page.py
+++ tests/test_frontend/test_landing_page.py
@@ -1,4 +1,4 @@
-from helium import Button, Link, start_firefox, kill_browser, Image
+from helium import Button, Link, start_firefox, kill_browser, Image, click, write, Text
 from selenium.common.exceptions import NoSuchElementException
 import pytest
 
@@ -12,27 +12,44 @@ def driver_home():
     kill_browser()
 
 
-def test_homepage_title(driver_home):
-    expected = "Strength Log"
-    assert driver_home.title == expected
+@pytest.fixture
+def driver_login():
+    driver = start_firefox(HOME_PAGE)
+    click(Link("Login"))
+    yield driver
+    kill_browser()
 
 
-def test_homepage_image(driver_home):
-    try:
-        Image("Maxes Page")
-    except NoSuchElementException:
-        pytest.fail("Maxes Page image missing.")
+class TestHomePage:
+    def test_title(self, driver_home):
+        expected = "Strength Log"
+        assert driver_home.title == expected
 
+    def test_image(self, driver_home):
+        try:
+            Image("Maxes Page")
+        except NoSuchElementException:
+            pytest.fail("Maxes Page image missing.")
 
-def test_homepage_has_login(driver_home):
-    try:
-        Link("Login")
-    except NoSuchElementException:
-        pytest.fail("Login element not found.")
+    def test_has_login(self, driver_home):
+        try:
+            Link("Login")
+        except NoSuchElementException:
+            pytest.fail("Login element not found.")
 
+    def test_get_started_button(self, driver_home):
+        try:
+            Button("Get Started")
+        except NoSuchElementException:
+            pytest.fail("Get Started button not found.")
 
-def test_homepage_get_started_button(driver_home):
-    try:
-        Button("Get Started")
-    except NoSuchElementException:
-        pytest.fail("Get Started button not found.")
+    def test_login(self, driver_home):
+        click(Link("Login"))
+        write("test@demo.com", into="Email")
+        write("test", into="Password")
+        click("Submit")
+
+        assert Text("Logs").exists()
+        assert Button("Log Workout").exists()
+        assert Button("Lift Maxes").exists()
+        assert Button("Log Out").exists()
