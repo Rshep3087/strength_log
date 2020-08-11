af0fa2a8e9fdbe9c2ceceb66d6d5c2d872f6c80b
Ryan Sheppard <rshep3087@gmail.com>
2020-08-10 20:07:52 -0500

    format max html and add filter form format max html and add Log Workout to the top removed unused endpoint from maxes User can filter maxes by start and end date

--- strength_log/maxes/forms.py
+++ strength_log/maxes/forms.py
@@ -1,9 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import (
-    SubmitField,
-    DecimalField,
-)
+from wtforms import SubmitField, DecimalField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
+from datetime import datetime
 
 
 class MaxesForm(FlaskForm):
@@ -11,5 +10,17 @@ class MaxesForm(FlaskForm):
     bench = DecimalField("Bench Max", validators=[DataRequired()], default=225.0)
     deadlift = DecimalField("Deadlift Max", validators=[DataRequired()], default=405.0)
     press = DecimalField("Press Max", validators=[DataRequired()], default=135.0)
-
     submit = SubmitField("Submit")
+
+
+class DateForm(FlaskForm):
+    start_date = DateField("Start Date", format="%Y-%m-%d", default=datetime.today)
+    end_date = DateField("End Date", format="%Y-%m-%d", default=datetime.today)
+    date_submit = SubmitField("Filter Charts")
+
+    def validate_on_submit(self):
+        result = super(DateForm, self).validate()
+        if self.start_date.data > self.end_date.data:
+            return False
+        else:
+            return result
