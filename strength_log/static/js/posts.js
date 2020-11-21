7abd1d6048a2d75bc92fb32238c7deecca418acd
Ryan Sheppard <Rshep3087@gmail.com>
2020-11-21 10:31:37 -0800

    Format JavaScript

--- strength_log/static/js/posts.js
+++ strength_log/static/js/posts.js
@@ -1,39 +1,34 @@
-let main_sets_id = 0;
-let accessory_sets_id = 0;
+let mainSetsID = 0;
+let accessorySetsID = 0;
 
 
 function remove() {
-    if (main_sets_id < 1) {
-
-    } else {
-        let set = document.getElementById("main-" + main_sets_id + "-form");
+    if (mainSetsID >= 1) {
+        let set = document.getElementById("main-" + mainSetsID + "-form");
         set.remove();
-
-        main_sets_id--;
+        mainSetsID--;
     }
-
 }
 
 
 function add() {
-    main_sets_id++; // increment main_sets_id to get a unique ID for the new element
+    mainSetsID++; // increment mainSetsID to get a unique ID for the new element
 
     let templateForm = document.getElementById("main-_-form");
-
     let newForm = templateForm.cloneNode(true);
 
-    newForm.setAttribute("id", newForm.getAttribute("id").replace("_", main_sets_id));
-    newForm.setAttribute("data-index", main_sets_id);
+    newForm.setAttribute("id", newForm.getAttribute("id").replace("_", mainSetsID));
+    newForm.setAttribute("data-index", mainSetsID);
 
     let inputElements = newForm.getElementsByTagName("input");
     let labelElement = newForm.getElementsByTagName("label");
 
-    labelElement[0].setAttribute("id", labelElement[0].getAttribute("id").replace("_", main_sets_id));
-    labelElement[0].innerHTML = "Set: " + (main_sets_id + 1).toString();
+    labelElement[0].setAttribute("id", labelElement[0].getAttribute("id").replace("_", mainSetsID));
+    labelElement[0].innerHTML = "Set: " + (mainSetsID + 1).toString();
 
     for (i = 0; i < inputElements.length; i++) {
-        inputElements[i].setAttribute("id", inputElements[i].getAttribute("id").replace("_", main_sets_id));
-        inputElements[i].setAttribute("name", inputElements[i].getAttribute("name").replace("_", main_sets_id));
+        inputElements[i].setAttribute("id", inputElements[i].getAttribute("id").replace("_", mainSetsID));
+        inputElements[i].setAttribute("name", inputElements[i].getAttribute("name").replace("_", mainSetsID));
         inputElements[i].setAttribute("type", "number");
     }
 
@@ -43,39 +38,39 @@ function add() {
 
     //append elements
     sets.appendChild(newForm);
-
 }
 
 function remove_accessory() {
-    if (accessory_sets_id < 1) {
-
-    } else {
-        let set = document.getElementById("accessories-" + accessory_sets_id + "-form");
+    if (accessorySetsID >= 1) {
+        let set = document.getElementById("accessories-" + accessorySetsID + "-form");
         set.remove();
-
-        accessory_sets_id--;
+        accessorySetsID--;
     }
-
 }
 
 function add_accessory() {
-    accessory_sets_id++; // increment accessory_sets_id to get a unique ID for the new element
+    accessorySetsID++; // increment accessorySetsID to get a unique ID for the new element
 
     let templateForm = document.getElementById("accessories-_-form");
 
     let newForm = templateForm.cloneNode(true);
 
-    newForm.setAttribute("id", newForm.getAttribute("id").replace("_", accessory_sets_id));
-    newForm.setAttribute("data-index", accessory_sets_id);
+    newForm.setAttribute("id", newForm.getAttribute("id").replace("_", accessorySetsID));
+    newForm.setAttribute("data-index", accessorySetsID);
 
     let inputElements = newForm.getElementsByTagName("input");
     let accessoryToAdd = document.getElementById("accessory-lift");
 
+    // let accessoryLifts = document.getElementById("accessory-lift").options
+
     inputElements[0].value = accessoryToAdd.value;
 
     for (i = 0; i < inputElements.length; i++) {
-        inputElements[i].setAttribute("id", inputElements[i].getAttribute("id").replace("_", accessory_sets_id));
-        inputElements[i].setAttribute("name", inputElements[i].getAttribute("name").replace("_", accessory_sets_id));
+        inputElements[i].setAttribute("id", inputElements[i].getAttribute("id").replace("_", accessorySetsID));
+        inputElements[i].setAttribute("name", inputElements[i].getAttribute("name").replace("_", accessorySetsID));
+        if (inputElements[i].readOnly != true) {
+            inputElements[i].setAttribute("type", "number");
+        }
     }
 
     newForm.setAttribute("class", "subform");
@@ -84,5 +79,4 @@ function add_accessory() {
 
     //append elements
     sets.appendChild(newForm);
-
 }
